e9f73e2e75bca3199f1eef9277b4d3b222b207b3
Anang Fachreza <fachrezanang@gmail.com>
2020-04-01 12:24:20 +0700

    initial-commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {Button} from '@chakra-ui/core'
 
+import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
+  	<Button>hai</Button>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
