fc47630dc466043b1048e72442047e97755b9554
Anang Fachreza <fachrezanang@gmail.com>
2020-04-04 20:12:18 +0700

    make 404 component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import {Box} from '@chakra-ui/core'
 
+import Title from './Title'
+
 const NotFound  = () => {
-	return <Box>hello from 404</Box>
+	return <Box position="absolute" top="50%" left="50%" transform="translate(-50%, -50%)"><Title>404 Not found</Title></Box>
 }
 
 export default NotFound 
